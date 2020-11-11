ca46df5fd69f5ea7037e60e42e746aa18b6e89e2
Jules Spender <juspende@student.42.fr>
2020-11-11 23:45:02 +0100

    Adding the prediction part
    Adding the prediction part

--- pipeline.py
+++ pipeline.py
@@ -52,7 +52,6 @@ labels = epochs.events[:, -1] - 2
 
 epochs_data_train = epochs.get_data()
 cv = ShuffleSplit(10, test_size=0.2, random_state=42)
-cv_split = cv.split(epochs_data_train)
 
 
 # Assemble a classifier
@@ -81,3 +80,39 @@ class_balance = max(class_balance, 1. - class_balance)
 print("LDA Classification accuracy: %f / Chance level: %f" % (np.mean(scores_lda), class_balance))
 print("LDA SHRINKED Classification accuracy: %f / Chance level: %f" % (np.mean(scores_ldashrinkage), class_balance))
 print("SVC Classification accuracy: %f / Chance level: %f" % (np.mean(scores_svc), class_balance))
+
+sfreq = raw.info['sfreq']
+w_length = int(sfreq * 0.5)   # running classifier: window length
+w_step = int(sfreq * 0.1)  # running classifier: window step size
+w_start = np.arange(0, epochs_data_train.shape[2] - w_length, w_step)
+
+scores_windows = []
+
+for train_idx, test_idx in cv:
+    y_train, y_test = labels[train_idx], labels[test_idx]
+
+    X_train = csp.fit_transform(epochs_data_train[train_idx], y_train)
+    X_test = csp.transform(epochs_data_train[test_idx])
+
+    # fit classifier
+    svc.fit(X_train, y_train)
+
+    # running classifier: test classifier on sliding window
+    score_this_window = []
+    for n in w_start:
+        X_test = csp.transform(epochs_data_train[test_idx][:, :, n:(n + w_length)])
+        score_this_window.append(svc.score(X_test, y_test))
+    scores_windows.append(score_this_window)
+
+# Plot scores over time
+w_times = (w_start + w_length / 2.) / sfreq + epochs.tmin
+
+plt.figure()
+plt.plot(w_times, np.mean(scores_windows, 0), label='Score')
+plt.axvline(0, linestyle='--', color='k', label='Onset')
+plt.axhline(0.5, linestyle='-', color='k', label='Chance')
+plt.xlabel('time (s)')
+plt.ylabel('classification accuracy')
+plt.title('Classification score over time')
+plt.legend(loc='lower right')
+plt.show()
